93c37accb322c8a6e8cf28e03420af03572c9973
DRL7N <40870933+DRL7N@users.noreply.github.com>
2018-09-14 15:29:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -331,25 +331,23 @@ coll.first().delete()
  
   }
 })
-
 client.on('ready', function(){
-  client.user.setStatus("dnd");
-    var ms = 40000 ;	
-    var i = -1;	
-    var j = 0;	
-    setInterval(function (){	
-        if( i == -1 ){	
-j = 1;	
-       }	
-        if( i == (setGame.length)-1 ){	
-            j = -1;	
-      }	
-       i = i+j;	
-        client.user.setGame(setGame[i],`http://www.youtube.com/gg`);	
-}, ms);	
-	
-});
+    var ms = 10000 ;
+    var setGame = ['!play','!stop','T.'];
+    var i = -1;
+    var j = 0;
+    setInterval(function (){
+        if( i == -1 ){
+            j = 1;
+        }
+        if( i == (setGame.length)-1 ){
+            j = -1;
+        }
+        i = i+j;
+        client.user.setGame(setGame[i],`https://www.twitch.tv/tee`);
+    }, ms);
 
+})
 client.on('message' , message => {
   var prefix = "!";
   if(message.author.bot) return;
